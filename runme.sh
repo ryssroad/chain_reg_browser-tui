bfdb8b46c251886d6b9f873a49ea3a17c354fcff
ryssroad <ryssroad@gmail.com>
2023-10-06 21:56:34 +0600

    synt fixes

--- runme.sh
+++ runme.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Display a cool message
-echo "Yo, dude! We're about to download and run the Chain Registry Browser TUI 🤖 "
+echo "Yo, dude! We're about to download and run the Chain Registry Browser TUI "
 read -p "Are you totally sure you wanna do this? (Y/n): " answer
 
 if [ "$answer" != "n" ]; then
     # Clone the repo
-    echo "Nice! Cloning the repo... ☢️"
+    echo "Nice! Cloning the repo..."
     git clone https://github.com/ryssroad/testnet_chain_reg_browser-tui
     cd testnet_chain_reg_browser-tui
 
     # Create a virtual environment
-    echo "Creating a virtual environment... 🚧"
+    echo "Creating a virtual environment..."
     python3 -m venv ./venv
     source venv/bin/activate
 
     # Install dependencies
-    echo "Installing dependencies... 🐲"
+    echo "Installing dependencies..."
     pip install -r requirements.txt
 
-    echo "Installation complete, bro! 🥇"
+    echo "Installation complete, bro!"
 else
-    echo "Installation canceled, no worries, dude. 🙏🏽"
+    echo "Installation canceled, no worries, dude."
 fi
